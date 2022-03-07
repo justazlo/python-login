37b381742760bc5d514b9e84e7c87388373d9c55
Kimble <kimble1337@hotmail.com>
2022-03-07 22:58:40 +0000

    login.py

---
+++ login.py
@@ -0,0 +1,11 @@
+import time
+
+username=input("Enter Username: ")
+password=input("Enter Password: ")
+if password=="[37;1mX}Mr]H6f+~^2?Fk87}62Sq?.4" and username=="admin":
+    print("Successfully logged in!")
+    time.sleep(3)
+    print("Hello, World!")
+else:
+    print("Please Try Again!")
+    time.sleep(3)
